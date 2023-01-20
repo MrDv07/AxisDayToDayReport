39d8cede922427cc7a055c9829d678261079036d
Divyanshu Tiwari <divyanshutiwari9999@gmail.com>
2023-01-20 16:10:27 +0530

    TASK-1

---
+++ Day18-Task-1-kotlin.kt
@@ -0,0 +1,33 @@
+class college {
+    var collegeName:String = "HCET"
+    var collegePlace:String = "JBP"
+
+    fun display(){
+        println("$collegeName $collegePlace")
+    }
+
+    inner class branch{
+        var branchName:String = "CIVIL"
+        var branchPlace:String = "JBP"
+
+        fun branchdisplay(){
+            println("$branchName $branchPlace")
+        }
+
+        inner class Student{
+            var studentName:String = "DV"
+            var studentPlace:String = "JBP"
+            fun studentdisplay(){
+                println("$studentName $studentPlace")
+            }
+        }
+    }
+}
+
+fun main() {
+    var colobj = college()
+    colobj.display()
+    colobj.branch().branchdisplay()
+    colobj.branch().Student().studentdisplay()
+
+}
