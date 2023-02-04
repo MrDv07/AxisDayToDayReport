5818712f39e0bb5327f20b18cdac9a6db9066a44
Divyanshu <divyanshutiwari9999@gmail.com>
2023-02-04 19:39:04 +0530

    CRUD Changes

--- RestCrudMongodb/src/main/java/com/axis/exception/GlobalException.java
+++ RestCrudMongodb/src/main/java/com/axis/exception/GlobalException.java
@@ -1,5 +1,7 @@
 package com.axis.exception;
 
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,20 +11,38 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalException {
 	
 	@ExceptionHandler(IdNotFoundException.class)
-    ResponseEntity<String> myException(IdNotFoundException exception){
-    return new ResponseEntity<String>(exception.getMsg(), HttpStatus.NOT_FOUND);
+    ResponseEntity<ErrorInfo> myException(IdNotFoundException exception){
+		
+		ErrorInfo errorInfo = new ErrorInfo();
+		errorInfo.setErrorMessage(exception.getMsg());
+		errorInfo.setStatus(HttpStatus.NOT_FOUND.toString());
+		errorInfo.setLocalDateTime(LocalDateTime.now());	
+		return new ResponseEntity<ErrorInfo>(errorInfo, HttpStatus.NOT_FOUND);
 	}
    
    
     @ExceptionHandler(InValidAgeException.class)
-	   ResponseEntity<String> myException(InValidAgeException exception1){
-	   return new ResponseEntity<String>(exception1.getMsg(), HttpStatus.NOT_FOUND);
+    	ResponseEntity<ErrorInfo> myException(InValidAgeException exception1){
+    	
+    	
+    	ErrorInfo errorInfo = new ErrorInfo();
+		errorInfo.setErrorMessage(exception1.getMsg());
+		errorInfo.setStatus(HttpStatus.BAD_REQUEST.toString());
+		errorInfo.setLocalDateTime(LocalDateTime.now());
+		
+		return new ResponseEntity<ErrorInfo>(errorInfo, HttpStatus.BAD_REQUEST);
 	      
    }
     
     
     @ExceptionHandler(InValidSalaryException.class)
-    ResponseEntity<String> myException(InValidSalaryException exception2){
-    	return new ResponseEntity<String>(exception2.getMsg() , HttpStatus.NOT_FOUND);
+    	ResponseEntity<ErrorInfo> myException(InValidSalaryException exception2){
+    	
+    	ErrorInfo errorInfo = new ErrorInfo();
+		errorInfo.setErrorMessage(exception2.getMsg());
+		errorInfo.setStatus(HttpStatus.BAD_REQUEST.toString());
+		errorInfo.setLocalDateTime(LocalDateTime.now());
+    	
+    	return new ResponseEntity<ErrorInfo>(errorInfo , HttpStatus.NOT_FOUND);
     }
 }
